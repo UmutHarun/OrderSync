7530c95232fc463496b07ae0e1f29b1c268bd72b
UmutHarunJS <harun123ors@gmail.com>
2024-09-10 14:49:03 +0300

    booking completed

---
+++ OrderSync.DataAccessLayer/Abstract/INotificationDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSync.DataAccessLayer.Abstract
+{
+    internal interface INotificationDal
+    {
+    }
+}
